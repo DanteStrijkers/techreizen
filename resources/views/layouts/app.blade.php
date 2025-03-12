e05bea9785ca6a608e72bf6f5ba590a1e6768708
JaanRogiers <jaan.rogiers@hotmail.com>
2025-03-13 00:58:06 +0100

    navbar update: use laravel routing instead of url()

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,13 +48,13 @@
     <div> <!-- Removed margin-top to allow natural scrolling -->
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('home') }}">
                     Home
                 </a>
                 <a class="navbar-brand" href="{{ url('/voorbeeldreis') }}">
                     Voorbeeldreis
                 </a>
-                <a class="navbar-brand" href="{{ url('/contact') }}">
+                <a class="navbar-brand" href="{{ route('contact.form') }}">
                     Contact
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -66,9 +66,6 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-                        <a href="{{ route('contact.form') }}">
-                            <!--contact -->
-                        </a>
 
                     </ul>
 
