456db33e213bd3dd9b819422c05b6afd4935548d
Elijah Hubrechts <157287348+Elanch2@users.noreply.github.com>
2025-03-20 11:27:28 +0100

    mail layout

--- resources/views/emails/post-mail.blade.php
+++ resources/views/emails/post-mail.blade.php
@@ -1,3 +1,8 @@
+
+<div information>
+    <p>dit was gestuurd door: {{ $data['name'] }}</p>
+    <p>met deze contact email: {{ $data['email'] }}</p>
+</div>
 <div>
     <p>{{ $data['message'] }}</p>
 </div>
