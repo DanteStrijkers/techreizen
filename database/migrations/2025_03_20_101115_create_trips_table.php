cfca5b1a70cabd4d34e3721d6095d5b1720fd957
JaanRogiers <jaan.rogiers@hotmail.com>
2025-03-20 11:13:25 +0100

    migrations

--- database/migrations/2025_03_20_101115_create_trips_table.php
+++ database/migrations/2025_03_20_101115_create_trips_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     /**g
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down(): voidg
     {
         Schema::dropIfExists('trips');
     }
