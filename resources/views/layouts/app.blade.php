25058a8fb6117799886f3da7d7a841d00505c4a5
JaanRogiers <jaan.rogiers@hotmail.com>
2025-03-27 23:56:47 +0100

    add optional sidebar in layout (for admin panel/dashboard)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
 
 <body>
     <div class= "fixed-top" style="z-index: 100">
-        <div class="d-flex flex-row bg-light">
+        <div class="d-flex flex-row">
             <div class="p-2"> <img src="{{ asset('images/ucll_logo.png') }}" class="rounded" alt="logo ucll"></div>
             <div class="d-flex flex-column">
                 <div class="pt-3">
@@ -47,7 +47,7 @@
                 <a class="navbar-brand" href="{{ route('admin.panel') }}">
                     Admin
                 </a>
-                
+
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -101,7 +101,21 @@
         </nav>
 
         <main class="py-4">
-            @yield('content')
+            <div class="container-fluid">
+                <div class="row overflow-y-auto" style="max-height: 60vh">
+                    @hasSection('sidebar')
+                        @yield('sidebar')
+                    @endif
+
+                    @if(View::hasSection('sidebar'))
+                        @yield('content')
+                    @else
+                        <div class="d-flex justify-content-center">
+                            @yield('content')
+                        </div>
+                    @endif
+                </div>
+            </div>
         </main>
     </div>
 </body>
