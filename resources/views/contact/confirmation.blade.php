40659658a93c0ad890cdb8ce20f101b444c1c135
JaanRogiers <jaan.rogiers@hotmail.com>
2025-03-28 01:25:24 +0100

    add language files and use them in the views

--- resources/views/contact/confirmation.blade.php
+++ resources/views/contact/confirmation.blade.php
@@ -4,16 +4,16 @@
     <div class="row justify-content-center p-2">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-success text-white">{{ __('Succesvol Verzonden') }}</div>
+                <div class="card-header bg-success text-white">{{ __('contact.confirmation.title') }}</div>
 
                 <div class="card-body text-center">
-                    <h5>{{ __('Hey') }} {{ $first_name }}, {{ __('je bericht is succesvol verzonden!') }}</h5>
-                    <p>{{ __('We nemen zo snel mogelijk contact met je op.') }}</p>
+                    <h5>{{ __('contact.confirmation.header', ['first_name' => $first_name]) }}</h5>
+                    <p>{{ __('contact.confirmation.message') }}</p>
                 </div>
                 <div class="row mb-3">
                     <div class="text-center">
                             <a href="{{ route('home') }}" class="btn btn-primary">
-                                {{ __('Terug naar home') }}
+                                {{ __('contact.confirmation.button') }}
                             </a>
                     </div>
                 </div>
