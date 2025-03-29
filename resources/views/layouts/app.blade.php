39f46c091951ccfda345e5485e406ab900d6bd7c
JaanRogiers <jaan.rogiers@hotmail.com>
2025-03-29 22:21:24 +0100

    switcher component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,6 +75,8 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <x-locale-switcher />
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
