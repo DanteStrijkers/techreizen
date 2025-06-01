902bfa54dd787fe7fd9eca6e1305942d35cb1970
JaanRogiers <jaan.rogiers@hotmail.com>
2025-06-01 23:29:44 +0200

    fix locale switcher not working on admin panel

--- resources/views/admin-panel.blade.php
+++ resources/views/admin-panel.blade.php
@@ -285,7 +285,6 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.datatables.net/2.2.2/js/dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
